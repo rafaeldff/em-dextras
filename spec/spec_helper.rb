b5a9384f3b2f3cf5adc14f76ac008f2d86c44649
Rafael de F. Ferreira <public@rafaelferreira.net>
2013-04-02 17:24:25 -0300

    Configure focus flag for guard rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,9 @@
 require './lib/em-dextras'
 
 require 'em-dextras/spec'
+
+RSpec.configure do |config|
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+end
